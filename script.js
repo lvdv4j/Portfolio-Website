f72c0e980d00ac2d61f587186edb78ab6055d9ae
Lizzon De Villiers <lvdv4j@gmail.com>
2024-09-26 14:09:35 +0200

    Added a backspace animation and an about section

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const texts = ["Lizzon-Vanique", "A programmer", "An animator", "A data engineer", "A game designer", "A website developer"];
-    const typingText = document.getElementById('changing-text');
-    let currentTextIndex = 0;
-  
-    function typeText(text, callback) {
+  const texts = ["Lizzon-Vanique", "A programmer", "An animator", "A data engineer", "A game designer", "A website developer", "A freelancer"];
+  const typingText = document.getElementById('changing-text');
+  const typingSpeed = 100; // Adjust typing speed here
+  const backspaceSpeed = 100; // Adjust backspace speed here
+  const backspacePauseTime = 1500; // Pause before backspacing starts
+  const typingPauseTime = 500; // Pause before typing the next text
+
+
+  let currentTextIndex = 0;
+
+  function typeText(text, callback) {
       let i = 0;
       typingText.textContent = '';
       const interval = setInterval(function () {
-        typingText.textContent += text.charAt(i);
-        i++;
-        if (i > text.length) {
-          clearInterval(interval);
-          setTimeout(callback, 1000); // Pause before starting next text
-        }
-      }, 100); // Adjust typing speed here
-    }
-  
-    function startTyping() {
+          typingText.textContent += text.charAt(i);
+          i++;
+          if (i > text.length) {
+              clearInterval(interval);
+              setTimeout(function () {
+                  backspaceText(text, callback); 
+              }, backspacePauseTime);
+          }
+      }, typingSpeed); 
+  }
+
+  function backspaceText(text, callback) {
+      let i = text.length;
+      const interval = setInterval(function () {
+          typingText.textContent = text.substring(0, i);
+          i--;
+          if (i < 0) {
+              clearInterval(interval);
+              setTimeout(callback, typingPauseTime);  
+          }
+      }, backspaceSpeed);  
+  }
+
+  function startTyping() {
       typeText(texts[currentTextIndex], function () {
-        currentTextIndex = (currentTextIndex + 1) % texts.length;
-        startTyping();
+          currentTextIndex = (currentTextIndex + 1) % texts.length;
+          startTyping();
       });
-    }
-  
-    startTyping();
-  });  
+  }
+
+  startTyping();
+});
